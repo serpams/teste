ff63ad6f99018022a627e65c06b9628e0e3fbdc9
serpams <eng.mserpa@gmail.com>
2022-04-06 15:45:30 -0300

    update

--- index.js
+++ index.js
@@ -1,178 +1,236 @@
-const makeWaSocket = require('@adiwajshing/baileys').default;
-const { delay, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion } = require('@adiwajshing/baileys');
-const P = require('pino');
-const { unlink, existsSync, mkdirSync, readFileSync } = require('fs');
-const express = require('express');
-const { body, validationResult } = require('express-validator');
-const cors = require('cors');
+const makeWaSocket = require("@adiwajshing/baileys").default;
+const {
+  delay,
+  useSingleFileAuthState,
+  DisconnectReason,
+  fetchLatestBaileysVersion,
+} = require("@adiwajshing/baileys");
+const P = require("pino");
+const { unlink, existsSync, mkdirSync, readFileSync } = require("fs");
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const cors = require("cors");
+const multer = require("multer");
 
-const http = require('http');
+const http = require("http");
 const port = process.env.PORT || 8000;
 const app = express();
 const server = http.createServer(app);
-const AuthPath = './BotSessions/';
-const Auth = 'auth_info.json';
+const AuthPath = "./BotSessions/";
+const Auth = "auth_info.json";
+
+// configure multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./assets/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
 app.use(express.json());
-app.use(cors({
-   origin: '*'
-}));
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 const BotUpdate = (socket) => {
-   socket.on('connection.update', ({ connection, lastDisconnect, qr }) => {
-      if (qr){
-         console.log('BOT - Qrcode: ', qr);
-      };
-      if (connection === 'close') {
-         const Reconnect = lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut
-         if (Reconnect) BOTConnection()
-         console.log(`BOT - CONEXÃO FECHADA! RAZÃO: ` + DisconnectReason.loggedOut.toString());
-         if (Reconnect === false) {
-            const removeAuth = AuthPath + Auth
-            unlink(removeAuth, err => {
-               if (err) throw err
-            })
-         }
+  socket.on("connection.update", ({ connection, lastDisconnect, qr }) => {
+    if (qr) {
+      console.log("BOT - Qrcode: ", qr);
+    }
+    if (connection === "close") {
+      const Reconnect =
+        lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
+      if (Reconnect) BOTConnection();
+      console.log(
+        `BOT - CONEXÃO FECHADA! RAZÃO: ` + DisconnectReason.loggedOut.toString()
+      );
+      if (Reconnect === false) {
+        const removeAuth = AuthPath + Auth;
+        unlink(removeAuth, (err) => {
+          if (err) throw err;
+        });
       }
-      if (connection === 'open'){
-         console.log('BOT -CONECTADO')
-      }
-   })
-}
+    }
+    if (connection === "open") {
+      console.log("BOT -CONECTADO");
+    }
+  });
+};
 
 const BOTConnection = async () => {
-   const { version } = await fetchLatestBaileysVersion()
-   if (!existsSync(AuthPath)) {
-      mkdirSync(AuthPath, { recursive: true });
-   }
-   const { saveState, state } = useSingleFileAuthState(AuthPath + Auth)
-   const config = {
-      auth: state,
-      logger: P({ level: 'error' }),
-      printQRInTerminal: true,
-      version,
-      connectTimeoutMs: 6000,
-      async getMessage(key) {
-         return { conversation: 'BOT' };
-      },
-   }
-   const socket = makeWaSocket(config);
-   BotUpdate(socket.ev);
-   socket.ev.on('creds.update', saveState);
+  const { version } = await fetchLatestBaileysVersion();
+  if (!existsSync(AuthPath)) {
+    mkdirSync(AuthPath, { recursive: true });
+  }
+  const { saveState, state } = useSingleFileAuthState(AuthPath + Auth);
+  const config = {
+    auth: state,
+    logger: P({ level: "error" }),
+    printQRInTerminal: true,
+    version,
+    connectTimeoutMs: 6000,
+    async getMessage(key) {
+      return { conversation: "BOT" };
+    },
+  };
+  const socket = makeWaSocket(config);
+  BotUpdate(socket.ev);
+  socket.ev.on("creds.update", saveState);
 
-   const BotSendMessage = async (jid, msg) => {
-      await socket.presenceSubscribe(jid)
-      await delay(2000)
-      await socket.sendPresenceUpdate('composing', jid)
-      await delay(2000)
-      await socket.sendPresenceUpdate('paused', jid)
-      await delay(1000)
-      return await socket.sendMessage(jid, msg)
+  const BotSendMessage = async (jid, msg) => {
+    await socket.presenceSubscribe(jid);
+    await delay(2000);
+    await socket.sendPresenceUpdate("composing", jid);
+    await delay(2000);
+    await socket.sendPresenceUpdate("paused", jid);
+    await delay(1000);
+    return await socket.sendMessage(jid, msg);
+  };
 
-   }
+  // Send message
 
-   // Send message
-
-
-
-
-   app.post('/text-message', [body('jid').notEmpty(),], async (req, res) => {
-      const errors = validationResult(req).formatWith(({ msg}) => {
-             return msg;
-      });
-      if (!errors.isEmpty()) {
-      return res.status(422).json({
-         status: false,
-         message: errors.mapped()
-      });
-      }
-   
-      const jid = req.body.jid.toString();
-      const message = req.body.message;
-      // const metadata = await socket.groupMetadata("La8QJc2n55IGP2GtHD2szA@g.us") 
-      // console.log(metadata)
-      // const metadata =await socket.groupMetadata("La8QJc2n55IGP2GtHD2szA@g.us") 
-      // console.log(metadata.id + ", title: " + metadata.subject + ", description: " + metadata.desc)
-      // const group = await socket.groupCreate("My Fab Group", [jid ,'5521977085506@s.whatsapp.net' ])
-      // console.log ("created group with id: " + group.gid + JSON.stringify(group))
-      // socket.sendMessage(group.id, { text: 'hello there' }) // say hello to everyone on the group
-
-         BotSendMessage(jid, { text: message }).then(response => {
-            res.status(200).json({
-               status: true,
-               response: response
-            });
-            }).catch(err => {
-            res.status(500).json({
-               status: false,
-               response: err
-            });
-            });
-
-   });
-
-   app.post('/sendlink', [
-      body('jid').notEmpty()
-   ], async (req, res) => {
-      const errors = validationResult(req).formatWith(({
-      msg
-      }) => {
+  app.post("/text-message", [body("jid").notEmpty()], async (req, res) => {
+    const errors = validationResult(req).formatWith(({ msg }) => {
       return msg;
+    });
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        status: false,
+        message: errors.mapped(),
+      });
+    }
+
+    const jid = req.body.jid.toString();
+    const message = req.body.message;
+    // const metadata = await socket.groupMetadata("La8QJc2n55IGP2GtHD2szA@g.us")
+    // console.log(metadata)
+    // const metadata =await socket.groupMetadata("La8QJc2n55IGP2GtHD2szA@g.us")
+    // console.log(metadata.id + ", title: " + metadata.subject + ", description: " + metadata.desc)
+    // const group = await socket.groupCreate("My Fab Group", [jid ,'5521977085506@s.whatsapp.net' ])
+    // console.log ("created group with id: " + group.gid + JSON.stringify(group))
+    // socket.sendMessage(group.id, { text: 'hello there' }) // say hello to everyone on the group
+
+    BotSendMessage(jid, { text: message })
+      .then((response) => {
+        res.status(200).json({
+          status: true,
+          response: response,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          status: false,
+          response: err,
+        });
+      });
+  });
+
+  app.post("/sendlink", [body("jid").notEmpty()], async (req, res) => {
+    const errors = validationResult(req).formatWith(({ msg }) => {
+      return msg;
+    });
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        status: false,
+        message: errors.mapped(),
+      });
+    }
+
+    const jid = req.body.jid;
+    const link = req.body.link;
+    const messagetoLink = req.body.messagetolink;
+    const title = req.body.title;
+    const description = req.body.description;
+
+    const templateMessage = {
+      forward: {
+        key: { fromMe: true },
+        message: {
+          extendedTextMessage: {
+            text: `*${messagetoLink}*: \n\n ${link}`,
+            matchedText: `${link}`,
+            canonicalUrl: `${link}`,
+            title: `${title}`,
+            description: `${description}`,
+            jpegThumbnail: readFileSync("./assets/cfc.jpg"),
+          },
+        },
+      },
+    };
+
+    BotSendMessage(jid, templateMessage)
+      .then((response) => {
+        res.status(200).json({
+          status: true,
+          response: response,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          status: false,
+          response: err,
+        });
+      });
+  });
+
+  // send image
+  app.post(
+    "/send-image",
+    [body("jid").notEmpty(), upload.single("image")],
+    async (req, res) => {
+      const errors = validationResult(req).formatWith(({ msg }) => {
+        return msg;
       });
       if (!errors.isEmpty()) {
-      return res.status(422).json({
-         status: false,
-         message: errors.mapped()
-      });
+        return res.status(422).json({
+          status: false,
+          message: errors.mapped(),
+        });
       }
-   
+
       const jid = req.body.jid;
-      const link = req.body.link;
-      const messagetoLink = req.body.messagetolink;
-      const title = req.body.title;
-      const description = req.body.description;
- 
-     const templateMessage =
-     {
-      forward: {
-         key: { fromMe: true },
-         message: {
-            extendedTextMessage: {
-               text:`*${messagetoLink}*: \n\n ${link}`,
-               matchedText:   `${link}` ,
-               canonicalUrl: `${link}` ,
-               title:  `${title}` ,
-               description:  `${description}` ,
-               jpegThumbnail:  readFileSync('./assets/cfc.jpg')
-            }
-         }
-      }
-   }
-     
-         BotSendMessage(jid,templateMessage).then(response => {
-            res.status(200).json({
-               status: true,
-               response: response
-            });
-            }).catch(err => {
-            res.status(500).json({
-               status: false,
-               response: err
-            });
-            });
-    
-      
-      
+      const image = req.file.path;
+      const message = req.body.message;
 
-   });
+      const buttons = [
+        { buttonId: "id1", buttonText: { displayText: "Button 1" }, type: 1 },
+      ];
 
-   app.get('getTotalGroup', )
-}
+      const buttonMessage = {
+        image: readFileSync("./assets/cfc.jpg"),
+        footerText: "Teste",
+        headerType: 4,
+      };
+      BotSendMessage(jid, buttonMessage)
+        .then((response) => {
+          res.status(200).json({
+            status: true,
+            response: response,
+          });
+        })
+        .catch((err) => {
+          res.status(500).json({
+            status: false,
+            response: err,
+          });
+        });
+    }
+  );
+  // create a route to recept image and send to whatsapp
+};
 
-BOTConnection()
+BOTConnection();
 
-server.listen(port, function() {
-   console.log('© BOT - Servidor rodando na porta: ' + port);
- });
+server.listen(port, function () {
+  console.log("© BOT - Servidor rodando na porta: " + port);
+});
